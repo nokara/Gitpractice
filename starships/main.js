91e8d11536d89806f57c93dcbe131438db67f496
nokara <karanosack@gmail.com>
2021-03-17 19:53:58 -0600

    Co-authored-by: Ben Shurts <benjaminshurts@gmail.com>

--- starships/main.js
+++ starships/main.js
@@ -1,24 +1,34 @@
 import { starships } from '../data/starships.js'
+//console.log(starships[3].name)
+
 
 const main = document.querySelector('main')
-const navList = document.querySelector('navList')
+const navList = document.getElementById('navList')
+//console.log(navList)
 const shipView = document.querySelector('.shipView')
 
-funciton populateNav() {
-    starships.forEach((starship) => {
+function populateNav() {
+    starships.forEach((starship, index) => {
+        // let i = index
+        // console.log(i)
         let listItem = document.createElement('li')
         listItem.textContent = starship.name
+        //console.log(listItem)
 
         navList.appendChild(listItem)
+        populateShipView(starship,index)
     })
 }
 
-function populateShipView(shipData){
-let shipImage = document.createElement('img')
-shipImage.src = `https://starwars-visualguide.com/assets/img/starships/10.jpg`
-shipView.appendChild(shipImage)
+function populateShipView(shipData,index){
+    let shipImage = document.createElement('img')
+    //et shipNum = shipData
+    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${index}.jpg`
+    shipView.appendChild(shipImage)
 }
 
 populateNav()
 
-populateShipView()
+//populateShipView()
+
+
