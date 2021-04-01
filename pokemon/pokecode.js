32fb758df5e5e06f3de302d3662f6ef9de28561c
nokara <karanosack@gmail.com>
2021-04-01 12:40:14 -0600

    congress page.js and started pokemon pages

---
+++ pokemon/pokecode.js
@@ -0,0 +1,36 @@
+const pokeGrid = document.querySelector('.pokeGrid')
+const loadButton = document.querySelector('.loadPokemon')
+
+loadButton.addEventListener('click', () => {
+    loadPage()
+})
+
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url) // try getting data from the API at the url provided
+        const data = await response.json() // convert the response into JSON
+        return data // return the data from the function to whoever called it
+    } catch (error) {
+        // must have been an error
+        console.log(error)
+    }
+}
+
+function loadPage() {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/7`).then(
+        (data) => {
+            for (const singlePokemon of data.results) {
+                populatePokeCard(singlePokemon)
+            }
+        }
+    )
+}
+
+function populatePokeCard(singlePokemon) {
+    //use same html as in the CodPen Card flip example
+    letpokeScene = document.createElement('div')
+    letpokeCard = document.createElement('div')
+    //make card front and card back
+    //append them all to the pokeGrid
+    console.log(singlePokemon)
+}
